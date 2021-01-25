224f0a2ce52afe59999996e03cd2ea93d6c37fae
Swagat Kumar Swain <swagat@cognissolutions.com>
2021-01-25 08:43:10 +0530

    loading static files

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, TestComponentComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [HelloComponent],
--- angular-demo/src/app/hello.component.ts
+++ angular-demo/src/app/hello.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
-  template: `<h1>Hello {{ name }}</h1>`,
+  template: `<h1>Hello {{ name }}</h1>
+  <app-test-component></app-test-component>`,
 })
 export class HelloComponent {
   name: string = 'John Smith';
