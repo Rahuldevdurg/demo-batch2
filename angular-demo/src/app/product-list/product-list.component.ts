5f89f0bee67ce6215864249ac832ef665ccef1bc
Swagat Kumar Swain <swagat@cognissolutions.com>
2021-01-26 08:45:06 +0530

    ngfor

--- angular-demo/src/app/product-list/product-list.component.ts
+++ angular-demo/src/app/product-list/product-list.component.ts
@@ -7,10 +7,95 @@ import { IProduct } from '../models/product.interface';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor() {}
   title: string = 'Product List';
+  products: IProduct[];
+  constructor() {
+    this.products = [
+      {
+        productName: 'Hero Honda CD 100',
+        description: 'Most popular Bike of India',
+        releaseDate: '10-08-1990',
+        price: 100,
+        isActive: true,
+        imageUrl: 'https://via.placeholder.com/150?text=CD100SS',
+      },
+      {
+        productName: 'Honda Hornet',
+        description: 'A sports Bike',
+        releaseDate: '10-08-2010',
+        price: 200,
+        isActive: true,
+        imageUrl: 'https://via.placeholder.com/150?text=Hornet',
+      },
+      {
+        productName: 'Super splendor',
+        description: 'A Bike that the nation trusts',
+        releaseDate: '10-08-1980',
+        price: 75,
+        isActive: true,
 
-  products: IProduct[] = null;
+        imageUrl: 'https://via.placeholder.com/150?text=Splendor',
+      },
+      {
+        productName: 'Yamaha RX 100',
+        description: 'Nostalgic !',
+        releaseDate: '10-08-1987',
+        price: 122,
+        isActive: false,
+        imageUrl: 'https://via.placeholder.com/150?text=RX100',
+      },
+    ];
+  }
 
+  refreshData() {
+    console.log('Refreshing !!');
+    this.products = [
+      {
+        productName: 'Hero Honda CD 100',
+        description: 'Most popular Bike of India',
+        releaseDate: '10-08-1990',
+        price: 100,
+        isActive: true,
+        imageUrl: 'https://via.placeholder.com/150?text=CD100SS',
+      },
+      {
+        productName: 'Honda Hornet',
+        description: 'A sports Bike',
+        releaseDate: '10-08-2010',
+        price: 200,
+        isActive: true,
+        imageUrl: 'https://via.placeholder.com/150?text=Hornet',
+      },
+      {
+        productName: 'Super splendor',
+        description: 'A Bike that the nation trusts',
+        releaseDate: '10-08-1980',
+        price: 75,
+        isActive: true,
+
+        imageUrl: 'https://via.placeholder.com/150?text=Splendor',
+      },
+      {
+        productName: 'Yamaha RX 100',
+        description: 'Nostalgic !',
+        releaseDate: '10-08-1987',
+        price: 122,
+        isActive: false,
+        imageUrl: 'https://via.placeholder.com/150?text=RX100',
+      },
+      {
+        productName: 'Maruti ALTO',
+        description: 'Any Desc',
+        releaseDate: '10-08-1999',
+        price: 1220,
+        isActive: true,
+        imageUrl: 'https://via.placeholder.com/150?text=Alto',
+      },
+    ];
+  }
   ngOnInit(): void {}
+
+  trackByName(index: number, product: IProduct) {
+    return product.productName;
+  }
 }
