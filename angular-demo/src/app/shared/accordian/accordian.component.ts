69ec94f42ea60a9982dc4b81ddeb777e9b1b516b
Swagat Kumar Swain <swagat@cognissolutions.com>
2021-02-01 09:13:35 +0530

    ContentChild

--- angular-demo/src/app/shared/accordian/accordian.component.ts
+++ angular-demo/src/app/shared/accordian/accordian.component.ts
@@ -1,14 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-accordian',
   templateUrl: './accordian.component.html',
   styleUrls: ['./accordian.component.css'],
 })
-export class AccordianComponent implements OnInit {
+export class AccordianComponent
+  implements OnInit, AfterContentInit, AfterContentChecked {
   @Input() header: string;
   isShown: boolean = false;
   constructor() {}
+  ngAfterContentInit(): void {
+    console.log(
+      'Inside ngAfterContentInit AccordianComponent ',
+      this.goSomewhereButton?.nativeElement
+    );
+  }
+  ngAfterContentChecked(): void {
+    console.log(
+      'Inside ngAfterContentChecked AccordianComponent ',
+      this.goSomewhereButton?.nativeElement
+    );
+  }
+  @ContentChild('goSomewhere') goSomewhereButton: ElementRef;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('Inside OnInit AccordianComponent ', this.goSomewhereButton);
+  }
 }
