9f8c22c632c0017b3368e40157c87ddafdea3166
Swagat Kumar Swain <swagat@cognissolutions.com>
2021-02-08 08:52:35 +0530

    error messages in reactive form

--- angular-demo/src/app/reactive-form/reactive-form.component.ts
+++ angular-demo/src/app/reactive-form/reactive-form.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
+  customerForm: FormGroup;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.customerForm = new FormGroup({
+    //   fullName: new FormControl('Swagat Swain'),
+    //   emailAddress: new FormControl(),
+    // });
+
+    this.customerForm = this.fb.group({
+      fullName: ['Swagat', Validators.required],
+      emailAddress: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  registerUser() {
+    console.log('Submitting Form ', this.customerForm);
+  }
 }
--- angular-demo/src/app/shared.module.ts
+++ angular-demo/src/app/shared.module.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomDatePipe } from './pipes/custom-date.pipe';
 import { IfNullOrEmptyPipe } from './pipes/if-null-or-empty.pipe';
 import { SearchProductPipe } from './pipes/search-product.pipe';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [
     IfNullOrEmptyPipe,
     SearchProductPipe,
     CustomDatePipe,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [IfNullOrEmptyPipe, SearchProductPipe, CustomDatePipe],
   providers: [],
